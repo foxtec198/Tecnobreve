76e55d780f9b50da740c4a35699cfbd0be194e46
foxtec198 <foxtec198@gmail.com>
2024-08-20 18:19:02 -0300

    .

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from os import getenv
+
+app = Flask(__name__)
+
+@app.route('/rinele')
+def home():
+    return 'Teste'
+    
+if __name__ == '__main__':
+    port = int(getenv('PORT','3605'))
+    app.run(debug=False, host='0.0.0.0', port=port)
