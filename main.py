6d8b5158d15b73403c3c5de37ff43f511c3944e3
Guilherme Breve <64221923+foxtec198@users.noreply.github.com>
2024-08-20 21:01:58 -0300

    Atualizar o

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from os import getenv
+
+app = Flask(__name__)
+
+@app.route('/')
+def homer():
+    return render_template('index2.html')
+
+if __name__ == '__main__':
+    port = getenv('PORT','5000')
+    app.run(host='0.0.0.0', port=port)
