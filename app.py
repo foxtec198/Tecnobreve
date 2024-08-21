d3fd25b8d3399db60c93504949cd7ba277fe414a
foxtec198 <foxtec198@gmail.com>
2024-08-21 15:01:04 -0300

    .

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from os import getenv
+from time import strftime as st
 from datetime import datetime as dt
 import email.message as em
 import smtplib
@@ -10,11 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-# ====================================================================================
+# =================================================================================
 
-
-# RINELE - PSICOLOGA ===============================================================
-def enviar_email(nome, telefone, emailTo, dataEnvio = dt.now()):
+# RINELE - PSICOLOGA ==============================================================
+def enviar_email(nome, telefone, emailTo, dataEnvio = st('%d/%m/%Y %H:%M')):
     # PARAMETROS DE EMAIL
     html = f"""<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -27,7 +27,7 @@ def enviar_email(nome, telefone, emailTo, dataEnvio = dt.now()):
         <hr>
         <p><b>Nome:</b> {nome}</p>
         <p><b>Telefone:</b> {telefone}</p>
-        <p><b>Data do Pedido:</b> {dataEnvio.strftime('%d/%m/%Y %H:%M')}</p>
+        <p><b>Data do Pedido:</b> {dataEnvio}</p>
         <a href="https://api.whatsapp.com/send/?phone=55{telefone}">
             <button style="background: #a7c957; color: #fff; font-size: 16px;"> 
                 <img src="https://firebasestorage.googleapis.com/v0/b/choppmania-828ed.appspot.com/o/icons8-whatsapp-50.png?alt=media&token=addb7a5a-9cec-4cd3-907b-20ce5be75295" width= "15">
@@ -57,18 +57,16 @@ def enviar_email(nome, telefone, emailTo, dataEnvio = dt.now()):
     msg.set_payload(html)
     server.sendmail(msg['From'], msg['To'], msg.as_string().encode('utf-8'))
     server.quit()
-    print(f'Email enviado com sucesso para {emailTo}')
 
 @app.route('/rinele/')
 def rinele():
     return render_template('rinele.html')
 
-@app.route('/enviar/<nome>_<telefone>')
+@app.route('/rinele/enviar/<nome>_<telefone>')
 def enviar(nome, telefone):
     enviar_email(nome, telefone, 'psicologarinelemazaquatro@hotmail.com')
-    return render_template('index.html')
-
-# ====================================================================================
+    return redirect('/rinele')
+# ==================================================================================
 
 if __name__ == '__main__':
     port = getenv('PORT','8601')
