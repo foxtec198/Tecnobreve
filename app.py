4fab7ca80e1e067c7a0024e59345c81050697a7b
foxtec198 <foxtec198@gmail.com>
2024-08-21 16:06:36 -0300

    .

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # HOME - TECNOBREVE ===============================================================
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return redirect('https://tecnobreve.my.canva.site/portifolio')
 # =================================================================================
 
 # RINELE - PSICOLOGA ==============================================================
